6e97da7b7c7a766772c4e877f08256e5e0953e42
Aaron Farr <theaaronfarr@gmail.com>
2022-12-03 21:56:46 -0700

    added dfs (no testing)

--- Graph.h
+++ Graph.h
@@ -6,7 +6,10 @@
 #define PROJECT1_STARTER_CODE_GRAPH_H
 
 #include <vector>;
+#include <algorithm>
 #include "Node.h"
+
+using namespace std;
 
 class Graph{
 private:
@@ -14,29 +17,61 @@ private:
     vector<Node*> revAdjList;
 
     vector<int> postOrder;
+    vector<int> preOrder;
     vector<vector<int>> sccList;
 
 public:
     void dfsForest(){
-        for(Node* : revAdjList){
-            if(node->isVisited == false){
-                
+        for(Node* node: revAdjList){
+            if(node->isVisited() == false){
+                vector<int> newGrouping;
+                newGrouping.push_back(node->getId());
+                Node* nextNode = revAdjList.at(node->getFirstAdj());
+
+                while(nextNode->hasAdjNode() && nextNode->isVisited() == false){
+                    newGrouping.push_back(nextNode->getId());
+                    nextNode = revAdjList.at(nextNode->getFirstAdj());
+                }
+
+                reverse(newGrouping.begin(), newGrouping.end());
+
+                for(int nodeId : newGrouping){
+                    postOrder.push_back(nodeId);
+                }
             }
         }
     }
 
-    void dfsForest(vector<int> postOrder){
-        
+    void dfsForestPostOrder(){
+        preOrder = postOrder;
+        reverse(preOrder.begin(), preOrder.end());
+
+        for(int nodeIt = 0; nodeIt < preOrder.size(); nodeIt++){
+            Node* node = adjList.at(preOrder.at(nodeIt));
+
+            if(node->isVisited() == false){
+                vector<int> newGrouping;
+                newGrouping.push_back(node->getId());
+                Node* nextNode = adjList.at(node->getFirstAdj());
+
+                while(nextNode->hasAdjNode() && nextNode->isVisited() == false){
+                    newGrouping.push_back(nextNode->getId());
+                    nextNode = adjList.at(nextNode->getFirstAdj());
+                }
+
+                sccList.push_back(newGrouping);
+            }
+        }
     }
 
     void populateReverseGraph(){
         for(Node* node : adjList){
-            revAdjList.insert(node->getId(), new Node(node->getId()));
+            revAdjList.insert(revAdjList.begin(),node->getId(),new Node(node->getId()));
         }
 
-        for(int i = 0; i < revAdjList.size(), i++){
-            for(int id : adjList.at(i)){
-                revAdjList.at(id).addAdjNode(i);
+        for(int i = 0; i < revAdjList.size(); i++){
+            for(int id = 0; id < revAdjList.at(i)->getAdjNodes().size(); id++){
+                revAdjList.at(id)->addAdjNode(i);
             }
 
         }
--- Node.h
+++ Node.h
@@ -6,32 +6,46 @@
 #define PROJECT1_STARTER_CODE_NODE_H
 
 #include <vector>
+#include <set>
+
+using namespace std;
 
 class Node{
 private:
     int id;
     bool visited;
+    bool sink;
     set<int> adjacentNodes;
 
 public:
-    Node() = default();
+    Node() = default;
     Node(int id){
         this->id = id;
+        this->sink = true;
     }
-    Node(int id, vector<int> adjacentNodes){
+    Node(int id, set<int> adjacentNodes){
         this->id = id;
         this->adjacentNodes = adjacentNodes;
+        this->sink = false;
     }
 
     bool isVisited() { return visited; }
+    bool hasAdjNode() { return this->sink; }
     int getId(){ return id; }
-    vector<int> getAdjNodes(){ return adjacentNodes; }
+    set<int> getAdjNodes(){ return adjacentNodes; }
 
-    void setVisited(bool visited){ this->visited = visited; }
-    void setId(int id){ this->id = id; }
-    void setAdjNodes(vector<int> nodes){ this->adjacentNodes = nodes; }
+    void setVisited(bool newVisited){ this->visited = visited; }
+    void setId(int newId){ this->id = id; }
+    void setAdjNodes(set<int> nodes){
+        this->adjacentNodes = nodes;
+        this->sink = false;
+    }
 
-    void addAdjNode(int nodeId){ this->adjacentNodes.insert(nodeId); }
+    void addAdjNode(int nodeId){
+        this->adjacentNodes.insert(nodeId);
+        this->sink = false;
+    }
+
     int getFirstAdj() { return *(this->adjacentNodes.begin()); }
 
 
