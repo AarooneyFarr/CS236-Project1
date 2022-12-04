8c6ac90a2e64583790d6a1173be77277fc34b214
Aaron Farr <theaaronfarr@gmail.com>
2022-12-03 19:46:25 -0700

    added

---
+++ Graph.h
@@ -0,0 +1,43 @@
+//
+// Created by TheAa on 12/3/2022.
+//
+
+#ifndef PROJECT1_STARTER_CODE_GRAPH_H
+#define PROJECT1_STARTER_CODE_GRAPH_H
+
+#include <vector>;
+#include "Node.h"
+
+class Graph{
+private:
+    vector<Node*> adjList;
+    vector<Node*> revAdjList;
+
+    vector<int> postOrder;
+    vector<vector<int>> sccList;
+
+public:
+    void dfsForest(){
+
+    }
+
+    void dfsForest(vector<int> postOrder){
+
+    }
+
+    void populateReverseGraph(){
+        for(Node* node : adjList){
+            revAdjList.insert(node->getId(), new Node(node->getId()));
+        }
+
+        for(int i = 0; i < revAdjList.size(), i++){
+            for(int id : adjList.at(i)){
+                revAdjList.at(id).addAdjNode(i);
+            }
+
+        }
+    }
+
+};
+
+#endif //PROJECT1_STARTER_CODE_GRAPH_H
---
+++ Node.h
@@ -0,0 +1,39 @@
+//
+// Created by TheAa on 12/3/2022.
+//
+
+#ifndef PROJECT1_STARTER_CODE_NODE_H
+#define PROJECT1_STARTER_CODE_NODE_H
+
+#include <vector>
+
+class Node{
+private:
+    int id;
+    bool visited;
+    set<int> adjacentNodes;
+
+public:
+    Node() = default();
+    Node(int id){
+        this->id = id;
+    }
+    Node(int id, vector<int> adjacentNodes){
+        this->id = id;
+        this->adjacentNodes = adjacentNodes;
+    }
+
+    bool isVisited() { return visited; }
+    int getId(){ return id; }
+    vector<int> getAdjNodes(){ return adjacentNodes; }
+
+    void setVisited(bool visited){ this->visited = visited; }
+    void setId(int id){ this->id = id; }
+    void setAdjNodes(vector<int> nodes){ this->adjacentNodes = nodes; }
+
+    void addAdjNode(int nodeId){ this->adjacentNodes.insert(nodeId); }
+
+
+};
+
+#endif //PROJECT1_STARTER_CODE_NODE_H
